5813ed70b2602f4d3fe4c859e273591b210a4327
V-Tamil <tamilselvi.venkatesan262@gmail.com>
2023-12-14 18:23:30 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let outputscreen = document.getElementById("outputscreen");
+function display(num) {
+  outputscreen.value += num;
+}
+function calculate() {
+  try {
+    outputscreen.value = eval(outputscreen.value);
+  } catch (err) {
+    alert("invalid");
+  }
+}
+function clr() {
+  outputscreen.value = " ";
+}
+function del() {
+  outputscreen.value = outputscreen.value.slice(0, -1);
+}
